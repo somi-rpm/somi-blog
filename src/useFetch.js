1a55ddfd7c95ed60b0912e20464934e8a19f92eb
Sourabh Mishra <sourabh.mishra201820@gmail.com>
2021-09-14 23:36:55 +0530

    Added Routing

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        setTimeout(() => {
-            fetch(url)
+        const abortCont = new AbortController();
+        setTimeout(() => { 
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 // console.log(res);
                 if (!res.ok){
@@ -21,10 +22,17 @@ const useFetch = (url) => {
                 setError(null)
             })
             .catch((err) => {
+                if(err.name === 'AbortError'){
+                    console.log('fetch aborted');
+                } else {
                 setIsPending(false)
                 setError(err.message)
+                }
             })
         }, 1000)
+
+        return () => abortCont.abort();
+
     }, [url]);
 
     return {data, isPending, error}
